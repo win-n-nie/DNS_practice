d49f2f0bb596442647d17d1031831c692dd1cb85
win-n-nie <102179077+win-n-nie@users.noreply.github.com>
2022-11-15 17:44:16 -0500

    update

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
-
+import requests
+from jinja2 import Template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def home_page():
+    return render_template('home_page.html')
 
-@app.route('/memorial')
-def bootstrap():
-    return render_template('memorial.html')
-    
+@app.route('/other/')
+def other_page():
+    return render_template('other_page.html')
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=80)
+    
